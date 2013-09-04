836d2edf65b9813ff151ef8f9b9f8c1c50e8b319
beyamor <beyamor@gmail.com>
2013-09-04 12:53:30 -0700

    Started selecting parents

--- src/evolute/app.cljs
+++ src/evolute/app.cljs
@@ -1,33 +1,24 @@
 (ns evolute.app
   (:require [evolute.music :as music]
             [evolute.song :as song]
-            [evolute.gui :as gui]
-            [dommy.core :as dommy])
-  (:use-macros [dommy.macros :only [sel1]]))
+            [evolute.gui :as gui]))
 
-(defn wrap-song-data
+(defn song-model
   [index song-data]
   {:title (str "Song " (inc index))
-   :data song-data})
+   :index index
+   :data song-data
+   :selected false})
 
-(defn hook-up-play-button
-  [song-node song]
-  (dommy/listen! (sel1 song-node ".play")
-    :click
-    #(music/play-song song
-                      :volume 100
-                      :time-signature [4 4]
-                      :tempo 180)))
-
-(defn create-song-node
-  [{:keys [data] :as song}]
-  (doto (gui/render-song song)
-    (hook-up-play-button data)))
+(defn create-initial-songs-model
+  []
+  (->>
+    song/random
+    (repeatedly 8)
+    (map-indexed song-model)
+    vec
+    atom))
 
 (set! (.-onload js/window)
-      #(->>
-         song/random
-         (repeatedly 8)
-         (map-indexed wrap-song-data)
-         (map create-song-node)
-         gui/show-songs))
+      #(let [songs-model (create-initial-songs-model)]
+         (gui/show-songs songs-model)))
--- src/evolute/gui.cljs
+++ src/evolute/gui.cljs
@@ -8,10 +8,39 @@
   (node
     [:div.song
      title
-     [:button.play {:type "button"} "Play"]]))
+     [:button.play {:type "button"} "Play"]
+     "Parent: " [:input.parent {:type "checkbox"}]]))
+
+(defn hook-up-play-button
+  [song-node song]
+  (dommy/listen!
+    (sel1 song-node ".play")
+    :click
+    #(music/play-song song
+                      :volume 100
+                      :time-signature [4 4]
+                      :tempo 180)))
+
+(defn hook-up-parent-button
+  [song-node songs index]
+  (dommy/listen!
+    (sel1 song-node ".parent")
+    :click
+    #(do
+       (if (.-checked (sel1 song-node ".parent"))
+         (dommy/add-class! song-node "parent")
+         (dommy/remove-class! song-node "parent"))
+       (swap! songs update-in [index :selected] not))))
+
+(defn create-song-node
+  [songs {:keys [data index] :as song}]
+  (doto (render-song song)
+    (hook-up-play-button data)
+    (hook-up-parent-button songs index)))
 
 (defn show-songs
-  [song-nodes]
+  [songs]
   (dommy/replace-contents!
     (sel1 :body)
-    (node [:div#songs song-nodes])))
+    (node [:div#songs
+           (map (partial create-song-node songs) @songs)])))
