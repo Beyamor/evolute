15ffaf233321029f9c6e676fe815be37bb461048
beyamor <beyamor@gmail.com>
2013-09-04 12:19:28 -0700

    Shuffling things around

--- src/evolute/app.cljs
+++ src/evolute/app.cljs
@@ -1,10 +1,33 @@
 (ns evolute.app
   (:require [evolute.music :as music]
             [evolute.song :as song]
-            [evolute.gui :as gui]))
+            [evolute.gui :as gui]
+            [dommy.core :as dommy])
+  (:use-macros [dommy.macros :only [sel1]]))
+
+(defn wrap-song-data
+  [index song-data]
+  {:title (str "Song " (inc index))
+   :data song-data})
+
+(defn hook-up-play-button
+  [song-node song]
+  (dommy/listen! (sel1 song-node ".play")
+    :click
+    #(music/play-song song
+                      :volume 100
+                      :time-signature [4 4]
+                      :tempo 180)))
+
+(defn create-song-node
+  [{:keys [data] :as song}]
+  (doto (gui/render-song song)
+    (hook-up-play-button data)))
 
 (set! (.-onload js/window)
       #(->>
          song/random
          (repeatedly 8)
+         (map-indexed wrap-song-data)
+         (map create-song-node)
          gui/show-songs))
--- src/evolute/gui.cljs
+++ src/evolute/gui.cljs
@@ -3,25 +3,15 @@
             [dommy.core :as dommy])
   (:use-macros [dommy.macros :only [node sel1]]))
 
-(defn play-button
-  [song] 
-  (doto (node [:button {:type "button"} "Play"])
-    (dommy/listen! :click
-                   #(music/play-song song
-                                     :volume 100
-                                     :time-signature [4 4]
-                                     :tempo 180))))
-
 (defn render-song
-  [index song]
+  [{:keys [title]}]
   (node
     [:div.song
-     (str "Song " (inc index))
-     (play-button song)]))
+     title
+     [:button.play {:type "button"} "Play"]]))
 
 (defn show-songs
-  [songs]
+  [song-nodes]
   (dommy/replace-contents!
     (sel1 :body)
-    (node [:div#songs
-           (map-indexed render-song songs)])))
+    (node [:div#songs song-nodes])))
